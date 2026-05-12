473e89c74125b27666d5b1dc454639830a216fa5
honghanh123456789 <hanhnguyenone2004@gmail.com>
2026-05-12 22:44:02 +0700

    ViewOrderTest.java

--- SeleniumOpenCart/src/test/java/ViewOrderTest.java
+++ SeleniumOpenCart/src/test/java/ViewOrderTest.java
@@ -29,28 +29,24 @@ public class ViewOrderTest {
         if (driver != null) driver.quit();
     }
 
-    // ===== LOGIN =====
+
     private void login(String email, String password) {
         driver.get(baseUrl + "index.php?route=account/login");
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-email"))).sendKeys(email);
         driver.findElement(By.id("input-password")).sendKeys(password);
-        // Click Login
+
         wait.until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//form[contains(@action,'login')]//button[@type='submit']")
         )).click();
 
-        // Verify login thành công
         wait.until(ExpectedConditions.urlContains("route=account/account"));
     }
 
-    // ===== CLICK MY ACCOUNT → ORDER HISTORY  =====
     private void OrderHistory() {
 
-        // ===== CLICK MY ACCOUNT  =====
         wait.until(ExpectedConditions.urlContains("account/account"));
 
-        // click Order History trong content page
         WebElement orderHistory = wait.until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//div[@id='content']//a[contains(@href,'route=account/order')]")
         ));
@@ -60,9 +56,9 @@ public class ViewOrderTest {
         wait.until(ExpectedConditions.urlContains("account/order"));
     }
 
-    // ==============================
+
     // TC01 – Có đơn hàng
-    // ==============================
+
     @Test
     public void TC01_viewOrderHistory_withOrders() {
 
@@ -76,9 +72,9 @@ public class ViewOrderTest {
         Assert.assertTrue(orders.size() > 0, "Không có đơn hàng");
     }
 
-    // ==============================
+
     // TC02 – Không có đơn hàng
-    // ==============================
+
     @Test
     public void TC02_viewOrderHistory_noOrders() {
 
@@ -93,9 +89,9 @@ public class ViewOrderTest {
         Assert.assertTrue(message.isDisplayed(), "Không hiển thị thông báo no orders");
     }
 
-    // ==============================
+
     // TC03 – Xem chi tiết đơn hàng
-    // ==============================
+
     @Test
     public void TC03_viewOrderDetail() {
 
@@ -108,19 +104,17 @@ public class ViewOrderTest {
         );
         Assert.assertTrue(orders.size() > 0, "Không có đơn hàng");
 
-        // click icon con mắt (View)
         WebElement viewBtn = wait.until(ExpectedConditions.presenceOfElementLocated(
                 By.xpath("//i[contains(@class,'fa-eye')]/parent::a")
         ));
 
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", viewBtn);
 
-        //  CHECK TRANG DETAIL
+
         WebElement orderDetailTable = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.cssSelector(".table.table-bordered")
         ));
-        // SCROLL XUỐNG CUỐI
-        // ===== SCROLL xuống  =====
+
         ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,700)");
 
         Assert.assertTrue(orderDetailTable.isDisplayed(), "Không hiển thị chi tiết đơn hàng");
